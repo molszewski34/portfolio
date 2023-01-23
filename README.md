7dde01f433735d8476bff55c999e0155d2b56555
molszewski34 <wpGatsbyMail@gmail.com>
2023-01-23 10:31:29 +0100

    changes in readme and index

--- README.md
+++ README.md
@@ -1,39 +1,36 @@
-<!-- AUTO-GENERATED-CONTENT:START (STARTER) -->
+# Portfolio
 
-<h1 align="center">
-  Portfolio
-</h1>
+This portfolio was made with use of Gatsbyjs and Headless Cms - Hygraph. Tailwind was used for practice reasons, although some custom css styles are also used. In gatsby-node.js file i used some custom schema injection for diffrent aproach to generating path to subsites made programaticaly. Today i would make it diffrent, but that was my way those days.
 
-Portfolio is a project made in Gatsbyjs to present latest projects.
-
-**Features.**
+## Features.
 Projects from repositories presented with programaticaly made pages.
 
-**Stack**
-React, Gatsbyjs, GraphCms, Tailwind
+## Stack
+React, Gatsbyjs, GraphCms, Tailwind, Headless Cms - Hygraph
 
-This portfolio was made with use of Gatsbyjs and React.
-Tailwind was used for practice reasons, although some custom css styles are also used. In gatsby-node.js file i used some custom schema injection for diffrent aproach to generating path to subsites made programaticaly. Today i would make it diffrent, but that was my way those days.
+## Dependencies:
+ "@formspree/react": "^2.4.0",
+ "@fortawesome/fontawesome-svg-core": "^6.2.0",
+ "@fortawesome/free-brands-svg-icons": "^6.2.0",
+ "@fortawesome/free-regular-svg-icons": "^6.2.0",
+  "@fortawesome/free-solid-svg-icons": "^6.2.0",
+  "@fortawesome/react-fontawesome": "^0.2.0",
+  "babel-plugin-macros": "^3.1.0",
+  "gatsby": "^4.21.1",
+  "gatsby-plugin-image": "^2.21.0",
+  "gatsby-plugin-manifest": "^4.21.0",
+  "gatsby-plugin-sharp": "^4.21.0",
+  "gatsby-source-filesystem": "^4.21.1",
+  "gatsby-source-graphcms": "^2.8.0",
+  "gatsby-source-graphql": "^4.21.0",
+  "gatsby-transformer-remark": "^5.23.0",
+  "gatsby-transformer-sharp": "^4.21.0",
+  "prop-types": "^15.8.1",
+  "react": "^18.1.0",
+  "react-dom": "^18.1.0"
 
-**Dependencies**
-"@formspree/react": "^2.4.0",
-"@fortawesome/fontawesome-svg-core": "^6.2.0",
-"@fortawesome/free-brands-svg-icons": "^6.2.0",
-"@fortawesome/free-regular-svg-icons": "^6.2.0",
-"@fortawesome/free-solid-svg-icons": "^6.2.0",
-"@fortawesome/react-fontawesome": "^0.2.0",
-"babel-plugin-macros": "^3.1.0",
-"gatsby": "^4.21.1",
-"gatsby-plugin-image": "^2.21.0",
-"gatsby-plugin-manifest": "^4.21.0",
-"gatsby-plugin-sharp": "^4.21.0",
-"gatsby-source-filesystem": "^4.21.1",
-"gatsby-source-graphcms": "^2.8.0",
-"gatsby-source-graphql": "^4.21.0",
-"gatsby-transformer-remark": "^5.23.0",
-"gatsby-transformer-sharp": "^4.21.0",
-"prop-types": "^15.8.1",
-"react": "^18.1.0",
-"react-dom": "^18.1.0"
 
-<!-- AUTO-GENERATED-CONTENT:END -->
+### This build use gh-pages and data from hygraph. To run it you need to create hygraph account and configure api on their service with endpoints of same name as designed in project. You also need to change name in graphql references.
+gatsby-node.js use .env file to hide api config with enviroment name: GRAPHCMS_ENDPOINT. For development use .env.development. Production use .env.production 
+
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -22,16 +22,15 @@ const IndexPage = ({
           GraphCMs
         </h3>
         <p className="text-base text-gray-500">
-          I am a Frontend developer with knowleadge about <b>Javascript</b>,
-          <b>React</b> , <b>GatsbyJS</b>, <b>Graphql </b>and
-          <b>headless CMS's</b>. I also use CSS frameworks as <b>Tailwind</b>
-          and <b>Bulma.</b>
+          I am a Frontend developer with knowleadge about <b>Javascript</b>,{" "}
+          <b>React</b>, <b>GatsbyJS</b>, <b>Graphql</b> and{" "}
+          <b>headless CMS's</b>. I also use CSS frameworks as <b>Tailwind</b> and <b>Bulma.</b>
         </p>
         <span className="text-gray-600 underline">
-          <Link to="/portfolio">
-            If your are intrested in how this page was made and my work with
-            Rich Text you can click <b className="text-blue-600">here.</b>
-          </Link>
+          <a href="https://github.com/molszewski34/portfolio">
+            If your are intrested in how this page was made, you can check my
+            repository <b className="text-blue-600">here.</b>
+          </a>
         </span>
         <button></button>
       </div>
@@ -42,68 +41,76 @@ const IndexPage = ({
         <h2 className="font-bold text-3xl sm:text-5xl">My Projects</h2>
       </header>
       <div className="">
-      {edges.map((page, index) => {
-        const context = page.node.context.data
-        return (
-          <article
-            key={index}
-            className="p-3 rounded mb-3 bg-black bg-opacity-80 flex flex-col justify-center items-center sm:max-w-2xl"
-          >
-            <a href={context.thumbnailAsset.url} target="_blank" rel="noreferrer" >
-              <div className="image">
-                <img
-                  src={context.thumbnailAsset.url}
-                  alt=""
-                  className="image__img"
-                />
+        {edges.map((page, index) => {
+          const context = page.node.context.data
+          return (
+            <article
+              key={index}
+              className="p-3 rounded mb-3 bg-black bg-opacity-80 flex flex-col justify-center items-center sm:max-w-2xl"
+            >
+              <a
+                href={context.thumbnailAsset.url}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <div className="image">
+                  <img
+                    src={context.thumbnailAsset.url}
+                    alt=""
+                    className="image__img"
+                  />
 
-                <div className="image__overlay" >
-                  <div className="image__title">
-                    <h4 className="image__text"> Click to Full view</h4>
+                  <div className="image__overlay">
+                    <div className="image__title">
+                      <h4 className="image__text"> Click to Full view</h4>
+                    </div>
                   </div>
                 </div>
-              </div>
               </a>
-            <div className="mt-3 flex flex-col  my-3 sm:p-3">
-              <Link
-                className="text-3xl font-bold text-center text-[#C94040] mb-3 hover:text-opacity-80"
-                to={page.node.path}
-                key={page.node.path}
-              >
-                {page.node.context.data.title}
-              </Link>
+              <div className="mt-3 flex flex-col  my-3 sm:p-3">
+                <Link
+                  className="text-3xl font-bold text-center text-[#C94040] mb-3 hover:text-opacity-80"
+                  to={page.node.path}
+                  key={page.node.path}
+                >
+                  {page.node.context.data.title}
+                </Link>
 
-              <p className="text-white">{context.description}</p>
-              {/* <hr className="border-red-500" /> */}
-              <p className="mt-3 text-left font-bold text-white">Features:</p>
-              <p className="text-gray-300">{context.features}</p>
-              <p className="text-left font-bold text-white">Stack:</p>
-              <p className="text-gray-300">{context.stack}</p>
-            </div>
-            <div className="flex flex-col w-full sm:flex-row sm:justify-between sm:px-6">
-              <Link className="  mt-3" to={page.node.path} key={page.node.path}>
-                <button className="bg-red-500 text-white p-3 font-semibold rounded-md hover:bg-opacity-80 ">
-                  Show Post
-                </button>
-              </Link>
+                <p className="text-white">{context.description}</p>
+                {/* <hr className="border-red-500" /> */}
+                <p className="mt-3 text-left font-bold text-white">Features:</p>
+                <p className="text-gray-300">{context.features}</p>
+                <p className="text-left font-bold text-white">Stack:</p>
+                <p className="text-gray-300">{context.stack}</p>
+              </div>
+              <div className="flex flex-col w-full sm:flex-row sm:justify-between sm:px-6">
+                <Link
+                  className="  mt-3"
+                  to={page.node.path}
+                  key={page.node.path}
+                >
+                  <button className="bg-red-500 text-white p-3 font-semibold rounded-md hover:bg-opacity-80 ">
+                    Show Post
+                  </button>
+                </Link>
 
-              <Link
-                className=" mt-3  "
-                to={page.node.context.data.githubLink}
-                key={page.node.context.data.githubLink}
-              >
-                <button className="bg-gray-500 text-white p-3 font-semibold flex items-center gap-1 rounded-md hover:bg-opacity-80">
-                  Code
-                  <FontAwesomeIcon
-                    className=" text-white text-2xl"
-                    icon={faGithub}
-                  />
-                </button>
-              </Link>
-            </div>
-          </article>
-        )
-      })}
+                <Link
+                  className=" mt-3  "
+                  to={page.node.context.data.githubLink}
+                  key={page.node.context.data.githubLink}
+                >
+                  <button className="bg-gray-500 text-white p-3 font-semibold flex items-center gap-1 rounded-md hover:bg-opacity-80">
+                    Code
+                    <FontAwesomeIcon
+                      className=" text-white text-2xl"
+                      icon={faGithub}
+                    />
+                  </button>
+                </Link>
+              </div>
+            </article>
+          )
+        })}
       </div>
     </section>
   </Layout>
@@ -112,8 +119,12 @@ const IndexPage = ({
 export const projectPages = graphql`
   query {
     allSitePage(
-      filter: {component: {eq: "G:/Frontend Portfolio/portfolio/src/templates/projectPost.js"}}
-      sort: {fields: context___data___date, order: DESC}
+      filter: {
+        component: {
+          eq: "G:/Frontend Portfolio/portfolio/src/templates/projectPost.js"
+        }
+      }
+      sort: { fields: context___data___date, order: DESC }
     ) {
       edges {
         node {
